fd72ce06ce6bd1d055051263fa553ebb4b073459
MajuMajer <20261ctb0100056@estudantes.ifpr.edu.br>
2026-04-11 17:02:41 +0000

    Aula 11 - 1019: convesao de tempo

---
+++ beecrowd/1006-media-2.py
@@ -0,0 +1,11 @@
+'''
+Problema: beecrowd | 1006
+Data: 2026.04.11
+Estudante: Maria Julia Pereira Majer
+'''
+
+A = float(input())
+B = float(input())
+C = float(input())
+media = (A * 2 + B * 3 + C * 5) / 10
+print(f"MEDIA = {media:.1f}")
