7d67c3c8b3933b5ba675944b554bfe3634cd8189
MajuMajer <20261ctb0100056@estudantes.ifpr.edu.br>
2026-04-11 15:51:22 +0000

    Aula 11 - 1005: media 1

---
+++ beecrowd/1008-salario.py
@@ -0,0 +1,11 @@
+'''
+Problema: beecrowd | 1008
+Data: 2026.04.11
+Estudante: Maria Julia Pereira Majer
+'''
+N = int(input())
+H = int(input())
+V = float(input())
+SAL = H * V
+print(f"NUMERO = {N}")
+print(f"SALARIO = U$ {SAL:.2f}")
